67c68b07e42126b35250cdf04615913839dfffcb
matlavv <mathis.laversin92@gmail.com>
2024-01-15 15:34:18 +0100

    Express routes

--- src/routes/CarRoute.js
+++ src/routes/CarRoute.js
@@ -1,5 +1,7 @@
+const express = require('express');
+const router = express.Router();
+import carController from "../controllers/CarController";
 
-import carController from "../controllers/CarController"
 /**
  * @swagger
  * components:
