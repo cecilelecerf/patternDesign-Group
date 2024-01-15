17b625c5c5d25fa5464a6e905bd1fe4fb585eaad
cecilelecerf <c.lecerf.cecile@gmail.com>
2024-01-15 14:45:50 +0100

    carModel

---
+++ src/models/CarModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+let carSchema = new Schema ({
+    releaseYear: {
+        type: Date,
+    },
+    salesNumber: {
+        type: Number,
+    },
+    brand : {
+        type : String,
+        required : true
+    },
+    model:{
+        type: String,
+        required : true,
+    },
+    doorsNumber:{
+        type : String,
+        required : true
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now()
+    },
+
+});
+
+module.exports = mongoose.model("Car", carSchema)
