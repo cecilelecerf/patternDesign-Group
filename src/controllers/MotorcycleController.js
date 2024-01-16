63fffa2a51e529d24d8ea269c6c154a9b4288d67
cecilelecerf <c.lecerf.cecile@gmail.com>
2024-01-16 11:25:58 +0100

    add a specific props in motorcycle model

--- src/controllers/MotorcycleController.js
+++ src/controllers/MotorcycleController.js
@@ -1,8 +1,9 @@
 // const Motorcycle =  require("../models/MotorcycleModel");
 const VehicleController = require("./VehicleController");
 class MotorcycleController extends VehicleController{
-    constructor(brand, model, releaseYear, salesNumber) {
+    constructor(brand, model, releaseYear, salesNumber, hasASideCar) {
         super(brand, model, releaseYear, salesNumber);
+        this.hasASideCar = hasASideCar;
     }
 };
 
--- src/models/MotorcycleModel.js
+++ src/models/MotorcycleModel.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 let motorcycleSchema = new Schema ({
+
+    hasSideCar:{
+        type : Boolean,
+        required : true,
+        default : false
+    },
 });
 
 const Motorcycle = Vehicle.discriminator("Motorcycle", motorcycleSchema)
