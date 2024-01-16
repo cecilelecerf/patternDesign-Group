64e2ca9e3088b8d55c04a735fc9db49deb3d6e31
Matlavv <mathis.laversin92@gmail.com>
2024-01-16 11:27:39 +0100

    clean

--- src/models/MotorcycleModel.js
+++ src/models/MotorcycleModel.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
-const Vehicle = require('./VehicleModel');
+const mongoose = require("mongoose");
 
-const motorcycleSchema = new mongoose.Schema({
-    hasSidecar: { type: Boolean, default: false }
+const Schema = mongoose.Schema;
+let motorcycleSchema = new Schema ({
+
+    hasSideCar:{
+        type : Boolean,
+        required : true,
+        default : false
+    },
 });
 
-const Motorcycle = Vehicle.discriminator('Motorcycle', motorcycleSchema);
-
-module.exports = Motorcycle;
+const Motorcycle = Vehicle.discriminator("Motorcycle", motorcycleSchema)
+module.exports = { Motorcycle }
